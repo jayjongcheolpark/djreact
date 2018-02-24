5cc86046f837ad83994e4fec829859889b6ea88e
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-24 18:35:30 -0500

    fix eslint warning

--- frontend/src/components/BigCard.js
+++ frontend/src/components/BigCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Rectangle, Circle, Ellipse, Line, Polyline, CornerBox, Triangle } from 'react-shapes'
 import 'tachyons'
 
 import './BigCard.css'
@@ -8,7 +7,7 @@ import SmallCard from './SmallCard'
 
 const BigCard = ({ gameTitle, question }) => {
   const renderCards = () =>
-    question.answers.map((answer, idx) => <SmallCard key={answer.text} gameTitle={gameTitle} answer={answer} />)
+    question.answers.map(answer => <SmallCard key={answer.text} gameTitle={gameTitle} answer={answer} />)
   return (
     <div className="w-100 center">
       <main className="w-100">
