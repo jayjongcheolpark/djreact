326d0d4583967b684624677b4f1860d5ef7758fc
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-24 02:08:46 -0500

    add main component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Main from './Main'
 import Card from './Card'
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
+          <Route exact path="/" component={Main} />
           <Route exact path="/:tableName/:qNum" component={Card} />
         </Switch>
       </BrowserRouter>
